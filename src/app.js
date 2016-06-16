1a407ec5f512287c1a2c8334a3f2bf95c46f2420
lwenke01 <lwenke@gmail.com>
2016-06-16 12:54:15 -0700

    First commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.css'
+import './app.css'
+import App from 'containers/App/App'
+
+
+
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
